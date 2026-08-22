c304ef2465f8f547a0a040868eba8cea070221fa
msarjun46 <mdsarjun46@gmail.com>
2026-08-23 03:27:18 +0530

    chore: remove accidentally tracked dummy migration and test scripts

--- test-mig.js
+++
@@ -1,9 +0,0 @@
-const { sql } = require('@payloadcms/db-postgres');
-const payload = require('payload');
-async function run() {
-  const p = await payload.getPayload({ config: require('./payload.config').default });
-  console.log('db keys:', Object.keys(p.db));
-  if (p.db.drizzle) console.log('drizzle keys:', Object.keys(p.db.drizzle));
-  process.exit(0);
-}
-run();
