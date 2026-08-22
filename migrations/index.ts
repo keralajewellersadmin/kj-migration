d8d59c97980e1f49bb4096ce2d28c821d2ef758e
msarjun46 <mdsarjun46@gmail.com>
2026-08-23 03:09:09 +0530

    fix: add missing image_id columns for site_settings arrays

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260822_add_rate_columns from './20260822_add_rate_columns';
+import * as migration_20260822_add_category_image from './20260822_add_category_image';
 
 export const migrations = [
   {
     up: migration_20260822_add_rate_columns.up,
     down: migration_20260822_add_rate_columns.down,
     name: '20260822_add_rate_columns'
+  },
+  {
+    up: migration_20260822_add_category_image.up,
+    down: migration_20260822_add_category_image.down,
+    name: '20260822_add_category_image'
   },
 ];
